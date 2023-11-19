c8bf23cec01e8bfeb504051c1b4f49aae6cbcf3e
astna17 <hei.antsa.17@gmail.com>
2023-11-19 14:17:52 +0300

    Create new class Topic

---
+++ Java_PostgreSQL/src/main/java/org/com/library/Topic.java
@@ -0,0 +1,13 @@
+package org.com.library;
+
+public class Topic {
+    private String topic;
+
+    public Topic(String topic) {
+        this.topic = topic;
+    }
+
+    public static Topic valueOf(String topic) {
+        return null;
+    }
+}
