7fc4a962cbf6800864142b9daa542fc07699837f
astna17 <hei.antsa.17@gmail.com>
2023-11-19 14:13:45 +0300

    Subscriber class which inherits the User

---
+++ Java_PostgreSQL/src/main/java/org/com/library/Subscriber.java
@@ -0,0 +1,18 @@
+package org.com.library;
+
+public class Subscriber extends User {
+    private String reference;
+    public Subscriber(int idSubscriber, String userName, String password, String reference) {
+        super(idSubscriber, userName, password);
+        this.reference = reference;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+}
